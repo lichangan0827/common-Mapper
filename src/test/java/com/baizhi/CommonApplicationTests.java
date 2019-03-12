d42b2e410a1f0f8f30b76e16728fa7494e3780de
L <1574917969@qq.com>
2019-03-12 17:20:48 +0800

    this is Project

---
+++ src/test/java/com/baizhi/CommonApplicationTests.java
@@ -0,0 +1,26 @@
+package com.baizhi;
+
+import com.baizhi.dao.AdminDao;
+import com.baizhi.entity.Admin;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonApplicationTests {
+    @Autowired
+    private AdminDao adminDao;
+    @Test
+    public void contextLoads() {
+        List<Admin> admins = adminDao.selectAll();
+        for (Admin admin : admins) {
+            System.out.println(admin);
+        }
+    }
+
+}
